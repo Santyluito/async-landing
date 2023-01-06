a5c229f0b697e0a7349b41cbd073afaa6fa853ca
Santiago <santyluito@gmail.com>
2023-01-06 14:15:19 -0500

    subida de landing page

---
+++ src/assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCCbLNE9AOGGMdQNRAt65OOQ&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '19680a2a38msh49821580443ffb3p1c2e46jsn6c54403a7ed5',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.tittle}
+                    </h3>
+                </div>
+            </div>
+        `).slice(0,4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
